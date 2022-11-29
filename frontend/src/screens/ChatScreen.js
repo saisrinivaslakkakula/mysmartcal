eb3e47f9fda82f148ee6ea1ac829b667e20d9df6
Sai Srinivas Lakkakula <saisrinivaslakkakula@sais-mbp.mynetworksettings.com>
2022-11-29 00:39:52 -0500

    rebasing commit

--- frontend/src/screens/ChatScreen.js
+++ frontend/src/screens/ChatScreen.js
@@ -8,19 +8,7 @@ const ChatScreen = () => {
     if(!userInfo){
         window.location.href = '/login'
     }
-    
-    const {id} = userInfo
-    const uid = id
-    // if user is not logged in, redirect to login page
-    
-        CometChat.login(uid, AUTH_KEY).then(
-            user => {
-                console.log("Login Successful:", { user });
-            },
-            error => {
-                console.log("Login failed with exception:", { error });
-            }
-        );
+// user is getting authenticated in cometchat once my app is asking for login to my app.
 
   return (
     <div>
