ab631dda0c912bf65ac3070d09aca65bac5b6e8e
Sai Srinivas Lakkakula <saisrinivaslakkakula@sais-mbp.mynetworksettings.com>
2022-12-02 03:07:00 -0500

    proxy changed back

--- backend/mysmartcal/src/main/java/com/mysmartcal/backend/kafka/WebSocketConfig.java
+++ backend/mysmartcal/src/main/java/com/mysmartcal/backend/kafka/WebSocketConfig.java
@@ -2,21 +2,30 @@ package com.mysmartcal.backend.kafka;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+import java.util.Collections;
 
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
+        CorsConfiguration configAutenticacao = new CorsConfiguration();
+        configAutenticacao.setAllowCredentials(false);
+        configAutenticacao.setAllowedOriginPatterns(Collections.singletonList("*"));
         // chat client will use this to connect to the server
         registry.addEndpoint("/ws-chat").setAllowedOriginPatterns("*").withSockJS();
     }
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
+        CorsConfiguration configAutenticacao = new CorsConfiguration();
+        configAutenticacao.setAllowCredentials(false);
+        configAutenticacao.setAllowedOriginPatterns(Collections.singletonList("*"));
         registry.setApplicationDestinationPrefixes("/app");
         registry.enableSimpleBroker("/topic/");
     }
